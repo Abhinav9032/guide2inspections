572bff9d34ca9c1a5f44b43731ff5ef9d7d4eefd
Abhinav Mishra <abhinav9032@gmail.com>
2021-12-25 16:37:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/hello/aws", (req, res) => {
   res.json({ message: "Ths is from aws deployment code pipeline testing" });
 });
 
-const PORT = 5000;
+const PORT = 8081;
 
 app.listen(PORT, () => {
   console.log(`SERVER WORKING AT ${PORT}`);
